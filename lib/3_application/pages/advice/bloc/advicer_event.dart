5e42716092e98179cf278bffb255f1073b25fe2e
Danish <danishmaulahasbi@gmail.com>
2022-12-15 10:46:12 +0700

    add equatable

--- lib/3_application/pages/advice/bloc/advicer_event.dart
+++ lib/3_application/pages/advice/bloc/advicer_event.dart
@@ -1,6 +1,12 @@
 part of 'advicer_bloc.dart';
 
 @immutable
-abstract class AdvicerEvent {}
+abstract class AdvicerEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
 
-class AdviceRequestedEvent extends AdvicerEvent {}
+}
+
+class AdviceRequestedEvent extends AdvicerEvent {
+
+}
